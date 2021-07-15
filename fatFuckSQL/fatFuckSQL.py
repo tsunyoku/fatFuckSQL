9c273059109536a324865361b3470b48c0195123
tsunyoku <tsunyoku@gmail.com>
2021-07-15 17:14:01 +0100

    add fatfawksql

--- fatFuckSQL/fatFuckSQL.py
+++ fatFuckSQL/fatFuckSQL.py
@@ -1,16 +1,13 @@
-import asyncio
+import aiomysql
 import asyncpg # troll
 
-class fatFuckSQL:
+class fatFuckSQL: #postgresql
     def __init__(self): # blank init vars
-        self._loop = None # _var because internal xd
         self._pool = None
 
     @classmethod
-    async def connect(self, db: str, host: str, password: str, user: str, loop = None):
+    async def connect(self, db: str, host: str, password: str, user: str):
         self = self()
-        
-        self._loop = loop or asyncio.get_event_loop()
         
         self._pool = await asyncpg.create_pool(
             user=user,
@@ -61,3 +58,54 @@ class fatFuckSQL:
 
     async def close(self):
         await self._pool.close()
+        
+class fatFawkSQL: # mysql/mariadb
+    def __init__(self):
+        self._pool = None
+        
+    async def connect(self, db: str, host: str, password: str, user: str):
+        self = self()
+        
+        self._pool = await aiomysql.create_pool(
+            user=user,
+            password=password,
+            db=db,
+            host=host,
+            autocommit=True
+        )
+        
+        return self
+        
+    async def fetch(self, query: str, params: list = []):
+        async with self._pool.acquire() as con:
+            async with con.cursor(aiomysql.DictCursor) as cur:
+                await cur.execute(query, params)
+                return await cur.fetchall()
+            
+    async def fetchval(self, query: str, params: list = []):
+        async with self._pool.acquire() as con:
+        async with con.cursor(aiomysql.DictCursor) as cur:
+            await cur.execute(query, params)
+            return list((await cur.fetchone()).values())[0] # return first value
+        
+    async def fetchrow(self, query: str, params: list = []):
+        async with self._pool.acquire() as con:
+            async with con.cursor(aiomysql.DictCursor) as cur:
+                await cur.execute(query, params)
+                return await cur.fetchone()
+            
+    async def execute(self, query: str, params: list = []):
+        async with self._pool.acquire() as con:
+            async with con.cursor(aiomysql.Cursor) as cur:
+                await cur.execute(query, params)
+                await conn.commit()
+                
+                return cur.lastrowid
+            
+    async def iter(self, query: str, params: list = []):
+        async with self._pool.acquire() as con:
+            async with con.cursor(aiomysql.DictCursor) as cur:
+                await cur.execute(query, params)
+                
+                async for row in cur:
+                    yield row
--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open('README.md', 'r') as rm:
 
 setuptools.setup(
     name='fatFuckSQL',
-    version='0.0.8',
+    version='0.0.9',
     author='tsunyoku',
     author_email='tsunyoku@gmail.com',
-    description='Light wrapper for asyncpg',
+    description='Light wrapper for asyncpg and aiomysql',
     long_description=ld,
     long_description_content_type='text/markdown',
     url='https://github.com/tsunyoku/fatFuckSQL',
     packages=setuptools.find_packages(),
-    install_requires=['asyncpg'],
+    install_requires=['asyncpg', 'aiomysql'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
